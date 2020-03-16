5541a13fe6407f6fcca8b42f8910eda4a4f97134
popovis <povarejkee@yandex.ru>
2020-03-16 18:55:32 +0300

    added auth form, installed formik yup

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import styles from './auth.module.css'
+import Button from '../../components/button'
 
 const Auth = () => {
-  return <div>Auth page</div>
+  const onSubmit = event => {
+    event.preventDefault()
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.auth}>
+        <h1 className={styles.title}>Авторизация</h1>
+        <form className={styles.form} onSubmit={onSubmit}>
+          <input type="text" />
+          <input type="text" />
+          <Button type="success">Войти</Button>
+          <Button type="primary">Регистрация</Button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Auth
